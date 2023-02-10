54498fb13f13aca5b7be646b0e18db379c97356f
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-02-10 20:19:40 +0200

    Add add, subtract, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
