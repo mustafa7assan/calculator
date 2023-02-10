82d58a5871bd4ee063d39f3ac05a723064f2d21b
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-02-10 20:23:24 +0200

    Add operate function which take operator and two number and return the result of the operation depend on the operator.

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+const operate = function (operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+};
