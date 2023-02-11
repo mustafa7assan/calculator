c268e0a757037a94fc99844f237b003924bd6312
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-02-11 21:20:55 +0200

    Add basic tow number calculation

--- script.js
+++ script.js
@@ -27,3 +27,35 @@ const operate = function (operator, a, b) {
       return divide(a, b);
   }
 };
+
+let firstNumber;
+let operatorSign;
+let secondNumber;
+
+const bottomDisplay = document.querySelector(".bottom-display");
+const topDisplay = document.querySelector(".top-display");
+bottomDisplay.textContent = "";
+
+const digits = document.querySelectorAll(".digits button");
+digits.forEach((button) => {
+  button.addEventListener("click", () => {
+    bottomDisplay.textContent += button.id;
+  });
+});
+
+const operators = document.querySelectorAll(".operators button");
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    firstNumber = Number(bottomDisplay.textContent);
+    bottomDisplay.textContent = "";
+    operatorSign = operator.id;
+    topDisplay.textContent = `${firstNumber} ${operatorSign}`;
+  });
+});
+
+const equal = document.getElementById("equal");
+equal.addEventListener("click", () => {
+  secondNumber = Number(bottomDisplay.textContent);
+  topDisplay.textContent += ` ${secondNumber} `;
+  bottomDisplay.textContent = operate(operatorSign, firstNumber, secondNumber);
+});
